736ab1f09823c3c907bda9e636d642b15377bec2
yonekawaisamu <i.yonekawa@willdo.co.jp>
2019-02-20 13:41:38 +0900

    追加予定機能の検討、コメント

--- employee/search.php
+++ employee/search.php
@@ -1,17 +1,24 @@
 <?php
 require('dbconnect.php');
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    header('Location: sign_in.php');
+    exit();
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    // フルネームの場合の処理を考える
     if ($search_word = filter_input(INPUT_POST, 'word', FILTER_SANITIZE_SPECIAL_CHARS)) {
         $search_word = '%' . $search_word . '%';
-        $records = $db->prepare('SELECT * FROM employees WHERE last_name LIKE ? OR first_name LIKE ? ORDER BY last_name ASC');
+        $records = $db->prepare('SELECT * FROM employees WHERE last_name LIKE ? OR first_name LIKE ? ORDER BY last_name DESC');
         $records->execute(array(
             $search_word,
             $search_word
         ));
     } else {
-        $records = $db->query('SELECT * FROM employees ORDER BY last_name ASC');
+        $records = $db->query('SELECT * FROM employees ORDER BY last_name DESC');
     }
 
 }
@@ -37,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <ul>
             <?php if (isset($records)): ?>
                 <?php while ($emps = $records->fetch()): ?>
-                    <li><a href="./show.php?employee_id=<?php echo $emps['id'] ?>"><?php echo htmlspecialchars($emps['last_name'] . ' ' . $emps['first_name']); ?></a></li>
+                    <li><a href="./show.php?id=<?php echo $emps['id'] ?>"><?php echo htmlspecialchars($emps['last_name'] . ' ' . $emps['first_name'], ENT_QUOTES, 'UTF-8'); ?></a></li>
                 <?php endwhile; ?>
             <?php endif; ?>
         </ul>
--- employee/show.php
+++ employee/show.php
@@ -1,3 +1,30 @@
+<?php
+require('dbconnect.php');
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    header('Location: sign_in.php');
+    exit();
+}
+
+if ($id = filter_input(INPUT_GET, 'id')) {
+    //社員検索
+    $emp_record = $db->prepare('SELECT * FROM employees where id=?');
+    $emp_record->execute(array($id));
+    $emp = $emp_record->fetch();
+
+    //出退勤履歴取得
+    $time_record = $db->prepare('SELECT * FROM time_record where employee_id=? ORDER BY date DESC, time DESC');
+    $time_record->execute(array($id));
+    var_dump($time_record->fetch());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +33,10 @@
 </head>
 <body>
     <h1>showです</h1>
+    <!-- 社員名編集、社員削除、出退勤履歴、を追加する -->
+    <?php if (isset($emp)): ?>
+        <?php echo $emp['last_name'] ?>
+        <?php echo $emp['first_name'] ?>
+    <?php endif ?>
 </body>
 </html>
